943198466aba5f66364689c070647f60783e5fd8
Autonyo Sanders <tonio.sanders@gmail.com>
2019-12-22 00:46:43 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,40 +1,48 @@
+require 'pry'
+
 class CashRegister
-  attr_accessor :total, :discount, :price, :items
 
-  def initialize(discount = 0)
-    @total = 0
-    @discount = discount
-    @items = []
-  end
-
-  def add_item(item, price, quantity = 1)
-    @price = price
-    @total += price * quantity
-    @quantity = quantity
-    if quantity > 1
-      counter = 0
-      while counter < quantity
-        @items << item
-        counter += 1
-      end
-    else
-      @items << item
+    attr_accessor :total, :discount, :total_items, :item, :price, :quantity, :last_transaction
+  
+    def initialize(discount = 0)
+       @total = 0
+       @discount = discount
+       @total_items = []
+     end
+  
+     def add_item(item, price, quantity = 1)
+         @item = item
+         @price = price
+        @quantity = quantity
+        @last_transaction = last_transaction
+         if quantity > 1
+            i = 0
+             while i < quantity
+               @total_items << item
+                 i+=1
+             end
+          else
+            @total_items << item
+          end
+         
+        @total += @price * @quantity
+     end
+     def apply_discount
+       if @discount  != 0
+         calc_discount = (self.discount/100.to_f)
+         discount_total = (self.total * calc_discount)
+         self.total  = self.total - discount_total
+  
+         "After the discount, the total comes to $#{self.total.to_i}."
+       elsif @discount == 0
+          "There is no discount to apply."
+       end
+     end
+     def items
+          self.total_items
+    end
+    def void_last_transaction
+        self.last_transaction = self.quantity * self.price
+        self.total = self.total - self.last_transaction
     end
   end
-
-  def apply_discount
-    if @discount > 0
-      @to_take_off = (price * discount)/100
-      @total -= @to_take_off
-      return "After the discount, the total comes to $#{total}."
-    else
-      return "There is no discount to apply."
-    end
-  end
-
-  def void_last_transaction
-    self.last_transaction = self.quantity - self.price
-    self.total = self.total - self.last_transaction
-  end
-
-end
