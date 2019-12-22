1142d2f076bbe6a97c0cc5081f8898fb86da41fe
Autonyo Sanders <tonio.sanders@gmail.com>
2019-12-22 00:44:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,6 +10,7 @@ class CashRegister
   def add_item(item, price, quantity = 1)
     @price = price
     @total += price * quantity
+    @quantity = quantity
     if quantity > 1
       counter = 0
       while counter < quantity
